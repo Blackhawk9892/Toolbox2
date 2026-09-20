c5f1f49b849cc8a07b1a79288cf96a117003c840
Blackhawk9892 <62289420+Blackhawk9892@users.noreply.github.com>
2026-09-19 21:36:58 -0500

    Update constants.php

--- includes/production/constants.php
+++ includes/production/constants.php
@@ -6,6 +6,7 @@
 	define("DB_USER","pfdstock_CmySqlConnection");
   
 	define("DB_SERVER_HOST","localhost");
+		
 	define(
     "OPENAI_API_KEY",
     "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"
