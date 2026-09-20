c0fbc2fdda69e62a71a3b0d6749efa2011e46e40
Blackhawk9892 <62289420+Blackhawk9892@users.noreply.github.com>
2026-09-19 21:42:58 -0500

    test

--- includes/production/constants.php
+++ includes/production/constants.php
@@ -5,7 +5,7 @@
 	define("DB_PASS","488c7f95c05989df6eab7107b7df8f625fe9c442");
 	define("DB_USER","pfdstock_CmySqlConnection");  
 	define("DB_SERVER_HOST","localhost");
-	define(
+		define(
     "OPENAI_API_KEY",
     "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"
 );
