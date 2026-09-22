2409bf01e5bcc8b17091bc7a20e525f3e11e84d8
Blackhawk9892 <62289420+Blackhawk9892@users.noreply.github.com>
2026-09-22 10:32:43 -0500

    remove ket

--- includes/constants.php
+++ includes/constants.php
@@ -4,6 +4,9 @@
 	
 	define("DB_USER","root");
 	define("DB_SERVER_HOST","localhost");
- 
+	define(
+    "OPENAI_API_KEY",
+    "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"
+);
 
-	?> 
+	?>
