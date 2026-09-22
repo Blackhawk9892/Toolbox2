b378dee933689242157573d9c8571fef96353cd4
Blackhawk9892 <62289420+Blackhawk9892@users.noreply.github.com>
2026-09-22 11:26:58 -0500

    reset constants

--- includes/production/constants.php
+++ includes/production/constants.php
@@ -9,5 +9,5 @@
     "OPENAI_API_KEY",
     "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"
 );
-	
+	 
 	?> 
